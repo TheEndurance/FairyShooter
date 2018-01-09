f22e12ed0eca63c002fb14d16fb735c22a20f470
Rawa Jalal <rawa.jalal@gmail.com>
2018-01-09 14:53:44 -0500

    documentation

--- FairyShooter/FairyNormalShootingBehaviour.cs
+++ FairyShooter/FairyNormalShootingBehaviour.cs
@@ -1,13 +1,26 @@
-﻿using Microsoft.Xna.Framework;
+﻿/* 
+ * Programmer: Rawa Jalal
+ * Revision History:
+ *          01/03/2017: Created
+ *          
+ */
+using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Input;
 using System;
 
 namespace FairyShooter
 {
+    /// <summary>
+    /// Represents normal fairy shooting behaviour
+    /// </summary>
     public class FairyNormalShootingBehaviour : IShootingBehaviour
     {
         public TimeSpan ShootInterval { get; }
         public TimeSpan? LastProjectileShot { get; set; }
+
+        /// <summary>
+        /// constructor for fairy normal shooting behaviour
+        /// </summary>
         public FairyNormalShootingBehaviour()
         {
             ShootInterval = TimeSpan.FromMilliseconds(255);
@@ -15,6 +28,12 @@ namespace FairyShooter
 
         }
 
+        /// <inheritdoc />
+        /// <summary>
+        /// </summary>
+        /// <param name="gameTime"></param>
+        /// <param name="projectileManager"></param>
+        /// <param name="shooter"></param>
         public void Shoot(GameTime gameTime, ProjectileManager projectileManager, Sprite shooter)
         {
            if (!shooter.IsDead)
--- FairyShooter/GameObjects.cs
+++ FairyShooter/GameObjects.cs
@@ -1,11 +1,17 @@
-﻿namespace FairyShooter
+﻿/* 
+ * Programmer: Rawa Jalal
+ * Revision History:
+ *          01/03/2017: Created
+ *          
+ */
+namespace FairyShooter
 {
+    /// <summary>
+    /// Holds all of the game objects
+    /// </summary>
     public class GameObjects
     {
-        public GameObjects()
-        {
 
-        }
         public Fairy Fairy { get; set; }
         public ProjectileManager ProjectileManager { get; set; }
         public EnemyManager EnemyManager { get; set; }
